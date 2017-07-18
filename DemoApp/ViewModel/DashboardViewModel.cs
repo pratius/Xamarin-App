d4ac4c853ca66202ef0a54504a89a583899a07d9
pratius kumar dubey <pratius.vin@gmail.com>
2017-07-18 16:10:10 +0400

    test

---
+++ DemoApp/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DemoApp.ViewModel
+{
+    public class DashboardViewModel:BaseViewModel
+    {
+        public DashboardViewModel(INavigation navigation):base(navigation)
+        {
+            
+        }
+    }
+}
