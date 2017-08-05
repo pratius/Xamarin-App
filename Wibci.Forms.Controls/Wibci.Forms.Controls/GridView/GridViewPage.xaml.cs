103dc03799bff4d4dc7a949089cbeb643af438bc
pratius.dubey <pratius.vin@gmail.com>
2017-08-05 22:36:48 +0530

    Add project files.

---
+++ Wibci.Forms.Controls/Wibci.Forms.Controls/GridView/GridViewPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wibci.Forms.Controls.GridView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridViewPage : ContentPage
+    {
+        private GridViewViewModel _viewModel;
+
+        public GridViewPage()
+        {
+            InitializeComponent();
+            _viewModel = new GridViewViewModel();
+            BindingContext = _viewModel; ;
+            Title = "GridView";
+        }
+
+        protected override void OnAppearing()
+        {
+            _viewModel.LoadData();
+        }
+    }
+}
---
+++ Wibci.Forms.Controls/Wibci.Forms.Controls/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
+using Wibci.Forms.Controls.GridView;
+using Xamarin.Forms;
+
+namespace Wibci.Forms.Controls
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public MainPageViewModel()
+        {
+            ShowGridViewPageCommand = new DelegateCommand(ShowGridViewPage);
+            ShowSwitchViewPageCommand = new DelegateCommand(ShowSwitchViewPage);
+        }
+
+        public ICommand ShowGridViewPageCommand { get; private set; }
+        public ICommand ShowSwitchViewPageCommand { get; private set; }
+
+        private async void ShowGridViewPage()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new GridViewPage());
+        }
+
+        private async void ShowSwitchViewPage()
+        {
+          ///  await Application.Current.MainPage.Navigation.PushAsync(new SwitchViewPage());
+        }
+    }
+}
